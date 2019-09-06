157489750d6ab4e8a3fc745b45fc83389b616e3f
knut-sdpark <sdp1422@naver.com>
2019-09-06 14:16:31 +0900

    first commit

---
+++ src/main/java/com/sdpark/example/oauthclientregister/OauthClientRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.sdpark.example.oauthclientregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientRegisterApplication.class, args);
+	}
+
+}
