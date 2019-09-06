176514a43c2136b5a75780a8dd41ad659ad15e96
knut-sdpark <sdp1422@naver.com>
2019-09-06 15:12:08 +0900

    created 'BasicClientInfo' class

---
+++ src/main/java/com/sdpark/example/oauthclientregister/client/BasicClientInfo.java
@@ -0,0 +1,35 @@
+package com.sdpark.example.oauthclientregister.client;
+
+public class BasicClientInfo {
+
+	private String name;
+
+	private String redirectUri;
+
+	private String clientType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRedirectUri() {
+		return redirectUri;
+	}
+
+	public void setRedirectUri(String redirectUri) {
+		this.redirectUri = redirectUri;
+	}
+
+	public String getClientType() {
+		return clientType;
+	}
+
+	public void setClientType(String clientType) {
+		this.clientType = clientType;
+	}
+
+}
